db4632f75a0c3996df10296de331b674b9c799e2
RJN2 <rosh.code@gmail.com>
2015-12-19 16:09:44 +0000

    added events

--- mIPL.js
+++ mIPL.js
@@ -1,7 +1,5 @@
 PlayersList = new Mongo.Collection('players');
 
-console.log("hello world");
-
 if (Meteor.isClient) {
   console.log("Hello client");
   Template.ipl_table.helpers({
@@ -10,6 +8,15 @@ if (Meteor.isClient) {
     },
     'playerCount': function(){
       return PlayersList.find().count();
+    }
+  });
+
+  Template.ipl_table.events({
+    'click .player': function(){
+      console.log("clicked");
+    },
+    'mouseover .player': function(){
+      console.log("mouseover");
     }
   });
 }
