16e08e25e57f6e6a64f9e7b684d2066a688932be
RJN2 <rosh.code@gmail.com>
2015-12-20 12:28:21 +0000

    add select player events

--- mIPL.js
+++ mIPL.js
@@ -1,26 +1,31 @@
 PlayersList = new Mongo.Collection('players');
 
 if (Meteor.isClient) {
-  console.log("Hello client");
-  Template.ipl_table.helpers({
-    'player': function(){
-      return PlayersList.find();
-    },
-    'playerCount': function(){
-      return PlayersList.find().count();
-    }
-  });
+	console.log("Hello client");
+	Template.ipl_table.helpers({
+		'player': function(){
+			return PlayersList.find()
+		},
+		'playerCount': function(){
+			return PlayersList.find().count()
+		},
+		'selectedClass': function(){
+			var playerID = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if (playerID == selectedPlayer) {
+				return "selected"
+			}
+		}
+	});
 
-  Template.ipl_table.events({
-    'click .player': function(){
-      console.log("clicked");
-    },
-    'mouseover .player': function(){
-      console.log("mouseover");
-    }
-  });
+	Template.ipl_table.events({
+		'click .player': function(){
+			var playerID = this._id;
+			Session.set('selectedPlayer', playerID);
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  console.log("Hello server");
+	console.log("Hello server");
 }
